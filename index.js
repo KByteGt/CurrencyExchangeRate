af6d64e391f49e58bb390b6aa762194d2225a8f4
Jose Lopez <30425866+JDByte01@users.noreply.github.com>
2021-04-16 10:37:17 -0600

    Making RESTful structur

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//Middlewares
+const logger = require('./middleware/logger');
+
+//Config
+const config = require('./config');
+
+//Routes
+const api = require('./routes/api');
+
+//Express
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use( (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
+    res.setHeader('Access-Control-Allow-Header','*')
+    next()
+});
+
+
+app.use('/api', api);
+
+app.use('/', (req,res) => {
+    res.status(400).json({'message': 'Invalid route'})
+});
+
+app.listen(config.server.port, config.server.ip, () => {
+    console.log('Server started at: ' + config.getDir)
+});

979b0867c902f2e15fd2275caa95477c3211438c
Jose Lopez <30425866+JDByte01@users.noreply.github.com>
2021-04-16 13:02:30 -0600

    Starting SOAP request

--- controllers/Exchange.js
+++ controllers/Exchange.js
@@ -1,14 +1,30 @@
+//https://www.google.com/finance/converter?a=1&from=USD&to='.$to
+
+const validateDate = date => {
+    const dateArray = date.split("-")
+
+    if(dateArray.length != 3) return false
+    if(dateArray[0] < 2015) return false
+    return dateArray
+}
+
+const validateCurrency = currency => {
+    return currency != "USD" && currency != "EU"
+}
+
+const banguatUrl = "https://www.banguat.gob.gt/variables/ws/TipoCambio.asmx";
+
 exports.getRate = (req, res, next) => {
     const currency = req.params.currency
-    const date = req.params.date
+    const date = validateDate(req.params.date)
 
-    if(currency != "USD" && currency != "EU"){
-        res.status(204).json({"message": "Currency "+ currency +" not supported"})
-    } else if(date.split("-")[0] < 2015){
-        res.status(204).json({"message": "Date "+ date +" not supported"})
+    if(validateCurrency(currency)){
+        res.status(404).json({"message": "Currency "+ req.params.currencyy +" not supported"})
+    } else if(!date){
+        res.status(404).json({"message": "Date "+ req.params.date +" not supported"})
     } else {
         res.status(200).json({
-            "date": date,
+            "date": req.params.date,
             "currency": currency,
             "rate": 7.74185
         })
@@ -16,12 +32,23 @@ exports.getRate = (req, res, next) => {
 }
 
 exports.getIntervalRate = (req, res, next) => {
-    res.status(200).json({
-        "start_date": req.params.date1,
-        "end_date": req.params.date2,
-        "currency": req.params.currency,
-        "mean": 7.746235,
-        "max": 7.75002,
-        "min": 7.74185
-    })
+
+    const currency = req.params.currency;
+    const date1 = validateDate(req.params.date1);
+    const date2 = validateDate(req.params.date2);
+
+    if(validateCurrency(currency)){
+        res.status(404).json({"message": "Currency "+ req.params.currency +" not supported"})
+    } else if(!date1 || !date2){
+        res.status(404).json({"message": "Dates "+ req.params.date1 +", "+ req.params.date2 +" not supported"})
+    }else {
+        res.status(200).json({
+            "start_date": req.params.date1,
+            "end_date": req.params.date2,
+            "currency": req.params.currency,
+            "mean": 7.746235,
+            "max": 7.75002,
+            "min": 7.74185
+        })
+    }
 }
--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,6 @@ const exchangeController = require('../controllers/Exchange');
 const api = express.Router();
 
 api.get('/exchange/rate/:date/:currency', exchangeController.getRate);
-
 api.get('/exchange/rate/:date1/:date2/:currency', exchangeController.getIntervalRate);
 
 module.exports = api;
